2f9afb02870c65869d1f68122b3ca55725ff0209
davidhager-ship-it <davidhager@paucasesnovescifp.cat>
2026-02-26 18:28:10 +0100

    Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,41 @@
-    /*
-     * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-     */
+package cuentas;
 
-    package cuentas;
+/**
+ * Clase principal que ejecuta la lógica de la aplicación bancaria.
+ * * @author David
+ */
+public class Main {
 
     /**
-     *
-     * @author David
+     * Método principal de entrada al programa
+     * @param args Argumentos de la línea de comandos
      */
-    public class Main {
+    public static void main(String[] args) {
+        Ccuenta cuenta1 = new Ccuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        operativa_cuenta(cuenta1, 695f);
+    }
 
-        public static void main(String[] args) {
-                    Ccuenta cuenta1 = new Ccuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-                    operativa_cuenta(cuenta1,695f);
-        }
+    /**
+     * Método que realiza las operaciones de retiro e ingreso sobre una cuenta
+     * @param cuenta1 El objeto cuenta sobre el que se opera
+     * @param cantidad La cantidad que se usará para el ingreso
+     */
     public static void operativa_cuenta(Ccuenta cuenta1, float cantidad){
-            double saldoActual;
+        double saldoActual;
 
-            saldoActual = cuenta1.estado();
-            System.out.println("El saldo actual es "+ saldoActual );
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
 
-            try {
-                cuenta1.retirar(2300);
-            } catch (Exception e) {
-                System.out.print("Fallo al retirar");
-            }
-            try {
-                System.out.println("Ingreso en cuenta");
-                cuenta1.ingresar(cantidad);
-            } catch (Exception e) {
-                System.out.print("Fallo al ingresar");
-            }
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
         }
     }
+}
